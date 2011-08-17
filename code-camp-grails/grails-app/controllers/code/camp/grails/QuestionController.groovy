0c8113fcf0ea46444c3ca07c5693b16ae90a1084
Stefan Glase <stefan.glase@googlemail.com>
2011-08-18 00:23:20 +0200

    formatting

--- code-camp-grails/grails-app/controllers/code/camp/grails/QuestionController.groovy
+++ code-camp-grails/grails-app/controllers/code/camp/grails/QuestionController.groovy
@@ -6,11 +6,11 @@ class QuestionController {
             /what currency did Spain use before the Euro/: { "Pesetas" },
             /what is ([0-9]+) plus ([0-9]+)/: { (it[0][1] as int) + (it[0][2] as int) },
             /what is ([0-9]+) multiplied by ([0-9]+)/: { (it[0][1] as int) * (it[0][2] as int) },
-            /which of the following numbers is the largest: (.+)/: { ((it[0][1] as String).split(", ") as List<Integer>).max() },
+            /which of the following numbers is the largest: (.+)/: { extractNumbers(it[0][1]).max() },
             /who played James Bond in the film Dr No/: { "Sean Connery" },
             /who is the Chancellor of Germany/: { "Angela Merkel" },
             /what is the ([0-9]+)th number in the Fibonacci sequence/: { fib((it[0][1] as int) - 1) },
-            /which of the following numbers are primes: (.+)/: { ((it[0][1] as String).split(", ") as List<Integer>).findAll{ (it as BigInteger).isProbablePrime(99) }.join(", ") }
+            /which of the following numbers are primes: (.+)/: { extractNumbers(it[0][1]).findAll { (it as BigInteger).isProbablePrime(99) }.join(", ") }
     ]
 
     def answer = {
@@ -25,7 +25,7 @@ class QuestionController {
         if (processor) {
             def answer = processor.value(question =~ processor.key)
             println "Answer: $answer"
-            render (answer as String)
+            render(answer as String)
         } else {
             render "Sorry, but I have no answer for your question: '$question'"
         }
@@ -38,6 +38,10 @@ class QuestionController {
 
     private int fib(int i) {
         i < 2 ? 1 : fib(i - 2) + fib(i - 1);
+    }
+
+    private List<Integer> extractNumbers(String string) {
+        string.split(", ") as List<Integer>
     }
 
 }
