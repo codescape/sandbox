15e8b25ea5269de850b91ad341a5d0581c86dfcb
Stefan Glase <stefan.glase@googlemail.com>
2011-09-01 10:36:47 +0200

    added sample project for m2eclipse workspace resolution problem

---
+++ m2eclipse-resolve-workspace/consumer/src/test/java/de/stefanglase/ConsumerTest.java
@@ -0,0 +1,14 @@
+package de.stefanglase;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ConsumerTest extends ProviderTest {
+
+    @Test
+    public void testSomethingDifferent() {
+        assertTrue(true);
+    }
+
+}
